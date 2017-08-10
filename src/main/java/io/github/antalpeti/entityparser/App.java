00ea40e5adbcc7a6ece8fd5c2d4320c556fc4495
Peti <antalpeti@users.noreply.github.com>
2017-08-10 20:59:43 +0200

    First commit

---
+++ src/main/java/io/github/antalpeti/entityparser/App.java
@@ -0,0 +1,13 @@
+package io.github.antalpeti.entityparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
