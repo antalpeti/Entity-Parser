79e41fda1e5ff6ce67947928e9b94ff20e6d840c
Peti <antalpeti@users.noreply.github.com>
2017-08-12 01:32:49 +0200

    Save and retrieve the last selected directory. #8

--- src/io/github/antalpeti/entityparser/Main.java
+++ src/io/github/antalpeti/entityparser/Main.java
@@ -1,15 +1,19 @@
 package io.github.antalpeti.entityparser;
 
 import java.io.File;
-import java.util.concurrent.Callable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 
 import io.github.antalpeti.entityparser.uielement.LabeledField;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,114 +27,166 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	private static double PREFERED_SIZE = 8192D;
-	private static String FONT_STYLE = "-fx-font: normal bold 15px 'serif' ";
-	
-	@Override
-	public void start(final Stage stage) {
-		
-		final LabeledField projectcodeLabeledField = createProjectcodeLabeledField();
-		
-		final TextArea outputTextArea = createOutputTextArea();
-        
-        final GridPane outputGridPane = createOutputGridPane(outputTextArea);
-		
-		final DirectoryChooser directoryChooser = new DirectoryChooser();
+  private static final double PREFERED_SIZE = 8192D;
+  private static final String FONT_STYLE = "-fx-font: normal bold 15px 'serif' ";
+  private static Properties properties = new Properties();
+  private static OutputStream outputStream = null;
+  private static InputStream inputStream = null;
 
-		final Button chooseEntityDirectoryButton = createChooseEntityDirectoryButton(stage, outputTextArea, directoryChooser);
+  @Override
+  public void start(final Stage stage) {
+    initStreams("config.properties");
 
-		final GridPane mainGridPane = createMainGridPane(projectcodeLabeledField, outputGridPane, chooseEntityDirectoryButton);
+    final LabeledField projectcodeLabeledField = createProjectcodeLabeledField();
 
-		final Pane mainPane = createMainPane(mainGridPane);
+    final TextArea outputTextArea = createOutputTextArea();
 
-		Scene scene = new Scene(mainPane, 800, 800);
-//		scene.getStylesheets().add("css/styles.css");
-		stage.setTitle("Entity Parser");
-		stage.setScene(scene);
-		stage.show();
-	}
+    final GridPane outputGridPane = createOutputGridPane(outputTextArea);
 
-	private LabeledField createProjectcodeLabeledField() {
-		Label projectcodeLabel = new Label("Projectcode:");
-		projectcodeLabel.setStyle(FONT_STYLE); 
-		final TextField projectcodeField = new TextField();
-		projectcodeField.setStyle(FONT_STYLE);
-		
-		final LabeledField projectcodeLabeledField = new LabeledField(5, projectcodeLabel, projectcodeField);
-		return projectcodeLabeledField;
-	}
-	
-	private TextArea createOutputTextArea() {
-		final TextArea outputArea = new TextArea();
-		outputArea.setWrapText(true);
-		outputArea.setStyle(FONT_STYLE);
-		outputArea.setPrefHeight(PREFERED_SIZE);
-		outputArea.setPrefWidth(PREFERED_SIZE);
-		outputArea.setMaxWidth(Double.MAX_VALUE);
-		outputArea.setMaxHeight(Double.MAX_VALUE);
-		return outputArea;
-	}
-	
-	private GridPane createOutputGridPane(final TextArea outputTextArea) {
-		GridPane outputGridPane = new GridPane();
-		GridPane.setVgrow(outputTextArea, Priority.ALWAYS);
-		GridPane.setHgrow(outputTextArea, Priority.ALWAYS);
-		outputGridPane.setMaxWidth(Double.MAX_VALUE);
-		outputGridPane.setMaxHeight(Double.MAX_VALUE);
-		Label outputLabel = new Label("Output:");
-		outputLabel.setStyle(FONT_STYLE); 
-		outputGridPane.add(outputLabel, 0, 0);
-		outputGridPane.add(outputTextArea, 0, 1);
-		return outputGridPane;
-	}
-	
-	private Button createChooseEntityDirectoryButton(final Stage stage, final TextArea outputTextArea,
-			final DirectoryChooser directoryChooser) {
-		final Button chooseEntityDirectoryButton = new Button("Choose Entity directory");
-		chooseEntityDirectoryButton.setStyle(FONT_STYLE);
-		chooseEntityDirectoryButton.setMaxWidth(PREFERED_SIZE);
-		chooseEntityDirectoryButton.setPrefWidth(PREFERED_SIZE);
-		chooseEntityDirectoryButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(final ActionEvent e) {
-				configureDirectoryChooser(directoryChooser);
-				final File selectedDirectory = directoryChooser.showDialog(stage);
-				
-				if (selectedDirectory == null) {
-					outputTextArea.setText("No Directory selected!");
-				} else {
-					outputTextArea.setText(selectedDirectory.getAbsolutePath());
-				}
-			}
-		});
-		return chooseEntityDirectoryButton;
-	}
-	
-	private static void configureDirectoryChooser(final DirectoryChooser directoryChooser) {
-		directoryChooser.setTitle("Choose Entity directory");
-		directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-	}
+    final DirectoryChooser directoryChooser = new DirectoryChooser();
 
-	private GridPane createMainGridPane(final LabeledField projectcodeLabeledField, final GridPane outputGridPane,
-			final Button chooseEntityDirectoryButton) {
-		final GridPane mainGridPane = new GridPane();
-		GridPane.setConstraints(projectcodeLabeledField, 0, 0);
-		GridPane.setConstraints(chooseEntityDirectoryButton, 0, 1);
-		GridPane.setConstraints(outputGridPane, 0, 2);
-		mainGridPane.setHgap(1);
-		mainGridPane.setVgap(1);
-		mainGridPane.getChildren().addAll(projectcodeLabeledField, chooseEntityDirectoryButton, outputGridPane);
-		return mainGridPane;
-	}
+    final Button chooseEntityDirectoryButton = createChoseEntityDirectoryButton(stage, outputTextArea, directoryChooser);
 
-	private Pane createMainPane(final GridPane mainGridPane) {
-		final Pane mainPane = new VBox(0);
-		mainPane.getChildren().addAll(mainGridPane);
-		mainPane.setPadding(new Insets(0, 0, 0, 0));
-		return mainPane;
-	}
+    final GridPane mainGridPane = createMainGridPane(projectcodeLabeledField, outputGridPane, chooseEntityDirectoryButton);
 
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
+    final Pane mainPane = createMainPane(mainGridPane);
+
+    Scene scene = new Scene(mainPane, 800, 800);
+    // scene.getStylesheets().add("css/styles.css");
+    stage.setTitle("Entity Parser");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  private void initStreams(String filePath) {
+    initOutputStream(filePath);
+    initInputStream(filePath);
+  }
+
+  private void initOutputStream(String filePath) {
+    try {
+      outputStream = new FileOutputStream(filePath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void initInputStream(String filePath) {
+    try {
+      inputStream = new FileInputStream(filePath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private LabeledField createProjectcodeLabeledField() {
+    Label projectcodeLabel = new Label("Projectcode:");
+    projectcodeLabel.setStyle(FONT_STYLE);
+    final TextField projectcodeField = new TextField();
+    projectcodeField.setStyle(FONT_STYLE);
+
+    final LabeledField projectcodeLabeledField = new LabeledField(5, projectcodeLabel, projectcodeField);
+    return projectcodeLabeledField;
+  }
+
+  private TextArea createOutputTextArea() {
+    final TextArea outputArea = new TextArea();
+    outputArea.setWrapText(true);
+    outputArea.setStyle(FONT_STYLE);
+    outputArea.setPrefHeight(PREFERED_SIZE);
+    outputArea.setPrefWidth(PREFERED_SIZE);
+    outputArea.setMaxWidth(Double.MAX_VALUE);
+    outputArea.setMaxHeight(Double.MAX_VALUE);
+    return outputArea;
+  }
+
+  private GridPane createOutputGridPane(final TextArea outputTextArea) {
+    GridPane outputGridPane = new GridPane();
+    GridPane.setVgrow(outputTextArea, Priority.ALWAYS);
+    GridPane.setHgrow(outputTextArea, Priority.ALWAYS);
+    outputGridPane.setMaxWidth(Double.MAX_VALUE);
+    outputGridPane.setMaxHeight(Double.MAX_VALUE);
+    Label outputLabel = new Label("Output:");
+    outputLabel.setStyle(FONT_STYLE);
+    outputGridPane.add(outputLabel, 0, 0);
+    outputGridPane.add(outputTextArea, 0, 1);
+    return outputGridPane;
+  }
+
+  private static final String CONFIG_PROPERTIES_LAST_SELECTED_DIRECTORY = "last.selected.directory";
+
+  private Button createChoseEntityDirectoryButton(final Stage stage, final TextArea outputTextArea, final DirectoryChooser directoryChooser) {
+    final Button chooseEntityDirectoryButton = new Button("Chose Entity directory");
+    chooseEntityDirectoryButton.setStyle(FONT_STYLE);
+    chooseEntityDirectoryButton.setMaxWidth(PREFERED_SIZE);
+    chooseEntityDirectoryButton.setPrefWidth(PREFERED_SIZE);
+    chooseEntityDirectoryButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(final ActionEvent e) {
+        configureDirectoryChooser(directoryChooser);
+        final File selectedDirectory = directoryChooser.showDialog(stage);
+        if (selectedDirectory == null) {
+          outputTextArea.setText("No Directory selected!");
+        } else {
+          outputTextArea.setText(selectedDirectory.getAbsolutePath());
+          storeProperties(selectedDirectory, "last.selected.directory", selectedDirectory.getAbsolutePath());
+        }
+      }
+
+    });
+    return chooseEntityDirectoryButton;
+  }
+
+  private void storeProperties(final File selectedDirectory, String key, String value) {
+    properties.setProperty(key, value);
+    try {
+      properties.store(outputStream, null);
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+  }
+
+  private static void configureDirectoryChooser(final DirectoryChooser directoryChooser) {
+    directoryChooser.setTitle("Choose Entity directory");
+
+    String lastSelectedDirectory = loadProperty(CONFIG_PROPERTIES_LAST_SELECTED_DIRECTORY);
+    if (lastSelectedDirectory != null) {
+      directoryChooser.setInitialDirectory(new File(lastSelectedDirectory));
+    } else {
+      directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+  }
+
+  private static String loadProperty(String key) {
+    try {
+      properties.load(inputStream);
+      return properties.getProperty(key);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private GridPane createMainGridPane(final LabeledField projectcodeLabeledField, final GridPane outputGridPane,
+      final Button chooseEntityDirectoryButton) {
+    final GridPane mainGridPane = new GridPane();
+    GridPane.setConstraints(projectcodeLabeledField, 0, 0);
+    GridPane.setConstraints(chooseEntityDirectoryButton, 0, 1);
+    GridPane.setConstraints(outputGridPane, 0, 2);
+    mainGridPane.setHgap(1);
+    mainGridPane.setVgap(1);
+    mainGridPane.getChildren().addAll(projectcodeLabeledField, chooseEntityDirectoryButton, outputGridPane);
+    return mainGridPane;
+  }
+
+  private Pane createMainPane(final GridPane mainGridPane) {
+    final Pane mainPane = new VBox(0);
+    mainPane.getChildren().addAll(mainGridPane);
+    mainPane.setPadding(new Insets(0, 0, 0, 0));
+    return mainPane;
+  }
+
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
 }
--- src/io/github/antalpeti/entityparser/uielement/LabeledField.java
+++ src/io/github/antalpeti/entityparser/uielement/LabeledField.java
@@ -1,7 +1,6 @@
 package io.github.antalpeti.entityparser.uielement;
 
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -9,35 +8,35 @@ import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
 
 public class LabeledField extends HBox {
-	
-	public LabeledField(double spacing, Label label, TextField textField) {
-		super(spacing);
-		getChildren().addAll(label, textField);
-		HBox.setHgrow(label, Priority.ALWAYS);
-		label.setMaxWidth(Double.MAX_VALUE);
-		HBox.setHgrow(textField, Priority.ALWAYS);
-		textField.setMaxWidth(Double.MAX_VALUE);
-	}
 
-	@Override
-	protected void layoutChildren() {
-		double minPrefWidth = calculatePrefChildWidth();
-		for (Node n : getChildren()) {
-			if (n instanceof Text) {
-				((Label) n).setMinWidth(minPrefWidth);
-			}
-			if (n instanceof TextField) {
-				((TextField) n).setMinWidth(minPrefWidth);
-			}
-		}
-		super.layoutChildren();
-	}
+  public LabeledField(double spacing, Label label, TextField textField) {
+    super(spacing);
+    getChildren().addAll(label, textField);
+    HBox.setHgrow(label, Priority.ALWAYS);
+    label.setMaxWidth(Double.MAX_VALUE);
+    HBox.setHgrow(textField, Priority.ALWAYS);
+    textField.setMaxWidth(Double.MAX_VALUE);
+  }
 
-	private double calculatePrefChildWidth() {
-		double minPrefWidth = 0;
-		for (Node n : getChildren()) {
-			minPrefWidth = Math.max(minPrefWidth, n.prefWidth(-1));
-		}
-		return minPrefWidth;
-	}
+  @Override
+  protected void layoutChildren() {
+    double minPrefWidth = calculatePrefChildWidth();
+    for (Node n : getChildren()) {
+      if (n instanceof Text) {
+        ((Label) n).setMinWidth(minPrefWidth);
+      }
+      if (n instanceof TextField) {
+        ((TextField) n).setMinWidth(minPrefWidth);
+      }
+    }
+    super.layoutChildren();
+  }
+
+  private double calculatePrefChildWidth() {
+    double minPrefWidth = 0;
+    for (Node n : getChildren()) {
+      minPrefWidth = Math.max(minPrefWidth, n.prefWidth(-1));
+    }
+    return minPrefWidth;
+  }
 }
